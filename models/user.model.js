c2b6f579588b12614dcb318274e505c63ae425fa
Fernando Lacaba <fernando.lacaba.herrera@gmail.com>
2019-04-17 21:26:33 +0200

    User model done

---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const SALT_WORK_FACTOR = 10;
+const FIRST_ADMIN_EMAIL = process.env.FIRST_ADMIN_EMAIL || 'admin@example.org';
+
+const userSchema = new mongoose.Schema({
+  name : {
+    type: String,
+    required: [true,'Name is required'],
+    unique: [true,'Name must be unique']
+  },
+  email : {
+    type: String,
+    required: [true,'Email is required'],
+    unique: [true,'Email must be unique'],
+    match : EMAIL_PATTERN,
+    trim: true,
+    lowercase : true
+  },
+  password : {
+    type: String,
+    required: [true,'Password is required'],
+    // Estaría bien meter aquí un match alfanumérico
+    minlength: [8, 'Password needs at last 8 chars']
+  },
+    avatarURL: String
+},{timestamps:true})
