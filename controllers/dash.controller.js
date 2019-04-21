1a76661be409f7b2bbd6e75a26b870b880520eb3
Fernando Lacaba <fernando.lacaba.herrera@gmail.com>
2019-04-21 12:43:44 +0200

    json response bbva api done

--- controllers/dash.controller.js
+++ controllers/dash.controller.js
@@ -1,9 +1,12 @@
+// esta pagina hay que estructurarla bien
+
+
 const axios = require('axios')
 const qs = require('qs')
 
 // change this
 const BBVA_AUTH = 'YXBwLmJidmEuaXJvbmhhY2s6aTdsNkp5UzhsQk4kYUpMR1RTa29UNDIzdyRWcXhnQk9pJDFGNVJjQGYkVDZJZip4YiRBbW0laFJqZFh0cVRRQg=='
-const bbvaTokenCall = {
+const bbvaTokenCall =  {
   method: 'POST',
   headers: { 
     'content-type': 'application/json',
@@ -11,8 +14,16 @@ const bbvaTokenCall = {
   },
   url:'https://connect.bbva.com/token?grant_type=client_credentials'
 }
-const bbvaApiCall = {
-  
+class BbvaApiCall {
+  constructor (accessToken,tokenType,type,maxDate,minDate) {
+    this.method = 'GET',
+    this.headers = {
+      'authorization': `${tokenType} ${accessToken}`,
+      'content-type': 'application/json'
+    }
+    //tengo que cambiar el zipcode en bruto por una variable
+    this.url = `https://apis.bbva.com/paystats_sbx/4/zipcodes/28002/origin_distribution?origin_type=${type}&max_date=${maxDate}&min_date=${minDate}`
+  }
 }
 
 
@@ -21,7 +32,16 @@ module.exports.setGraphs = (req,res,next) => {
 }
 
 module.exports.paintGraphs = (req,res,next) => {
-    axios(bbvaApiCall)
-      .then(response => res.send(response.data))
+  const {originType,maxDate,minDate} = req.query
+    axios(bbvaTokenCall)
+      .then(response => {
+        console.log(originType,maxDate,minDate)
+        const accessToken = response.data['access_token']
+        const tokenType = response.data['token_type']
+        const bbvaApi = new BbvaApiCall(accessToken,tokenType,originType,maxDate,minDate)
+        console.log(bbvaApi)
+        return axios(bbvaApi)
+          .then(response => res.send(response.data))
+      })
       .catch(next)
 }
