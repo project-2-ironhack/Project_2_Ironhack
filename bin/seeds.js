a1687b004aab16343b8d9fe2d478dcb7147ab3fb
Josue Valencia <josue.valencia@domo.ec>
2019-04-28 18:44:38 +0200

    postal codes from madrid

--- bin/seeds.js
+++ bin/seeds.js
@@ -6,37 +6,50 @@ const parce =  require('./../helpers/mapsParser'); //custom parse for google map
 // npm variables
 const output = process.env.npm_config_output || 'json'
 const what = process.env.npm_config_what || 'restaurants'
-const where = process.env.npm_config_where || 28045 //? working, but not searching with postal code
-const all = process.env.npm_config_all || false //! not working
+const where = process.env.npm_config_where || 'Madrid'
+const postalCode = require('./postalCodes');
+//! boolean not working
+const all = process.env.npm_config_all || false // search in deeper pages
+const restart = process.env.npm_config_all || true // delete previus data. 
+// ? searchig in all the postal codes. 
+const allData = process.env.npm_config_allData || false
+
+if(allData){
+  console.log(`Okey, dude, we will search in the ${postalCode.pc.length} postal codes from Madrid.`);
+} else {
+  console.log(`we are looking for ${what} in ${where} and we will look for more ${all}`) 
+}
 
 /* 
 * There is a short delay between when a next_page_token is issued, and when it will become valid. 
 * Requesting the next page before it is available will return an INVALID_REQUEST response. 
 * Retrying the request with the same next_page_token will return the next page of results.
  */
-const timeout = 2000
-console.log(`we are looking for ${what} in ${where} and we will look for more ${all}`) 
+const timeout = 1500
+
 
 
 // ** Connect to database. 
 require('./../config/db.config');
 
-//* Get data from GoogleMaps. 
-//?  npm run seeds --what='Restaurants' --where=28045 
-//! Postal code not working, if i send it by params. Why?? 
+// * Get data from GoogleMaps. 
+// ?  npm run seeds --what='Restaurants' --where=28045 
+// ! Postal code not working, if i send it by params. Why?? 
 // axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/${output}`, {
 //     params: {
-//       query : `${what}+in+${where}`,
+//       query : `${what}+in+${where}`, //? working, but not searching with postal code
 //       fields : 'geometry',
 //       key : GOOGLE_API,
 //     }
 //   })
 axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/${output}?query=${what}+in+${where}&fields=geometry&key=${GOOGLE_API}`)
   .then(response => {
+    // * First Response
     let page = response.data.results.length
     let data = parce.mapping(response.data.results);
     if(response.data.next_page_token) {
-      console.log('looking for pages 2')
+      // * Second Response
+      console.log(`looking in a second page for ${where}`)
       setTimeout(()=>{
         axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/${output}`, {         
           params: {
@@ -47,9 +60,9 @@ axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/${output}?query
           .then(nextRes => {
             page += nextRes.data.results.length
             data = [...data, ...parce.mapping(nextRes.data.results)]
-            // importMongo(data)
             if(nextRes.data.next_page_token) {
-              console.log('looking for page 3')
+              // * Third Response
+              console.log(`looking in a third for ${where}`)
               setTimeout(()=>{
                 axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/${output}`, {         
                   params: {
@@ -60,7 +73,7 @@ axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/${output}?query
                   .then(finalRes => {
                     data = [...data, ...parce.mapping(finalRes.data.results)]
                     page += finalRes.data.results.length            
-                    console.log(`Status ${finalRes.data.status}, we found:  ${page} elements in a third level`);
+                    console.log(`Status ${finalRes.data.status}, we found:  ${page} elements for the place ${where}`);
                     importMongo(data) 
                   })
                   .catch(err=>console.error(err))
@@ -80,15 +93,13 @@ axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/${output}?query
   })
   .catch(error => console.log(error));
 
-
-
 // ** Delete & Import to Mongo
 function importMongo(data) {
   console.log('preparing to import')
   Places.deleteMany()
     .then(() => Places.create(data))
     .then(inside => {
-        console.log(`Created ${inside.length} places`)
+        console.log(`Imported ${inside.length} places inside Mongo`)
         mongoose.connection.close()
     })
     .catch(e => console.error(e))
