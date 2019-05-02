f15c159ce8943eaf377bbaa45871d2e5b25ec4c1
Josue Valencia <josue.valencia@domo.ec>
2019-05-02 12:48:12 +0200

    array import working

--- bin/importPlaces.js
+++ bin/importPlaces.js
@@ -5,9 +5,11 @@ const Places = require('./../models/places.model');
 
 // ** Delete & Import to Mongo
 module.exports.importMongo = (data) => {
-    return  Places.create(data)
-              .then(imported => {
-                return imported ? imported.length : 0
-              })
+    return  (
+      Places.create(data)
+        .then(imported => {
+          return imported ? imported.length : 0
+        })
+    )
 }
 
--- bin/seeds.js
+++ bin/seeds.js
@@ -14,19 +14,21 @@ const allData = process.env.npm_config_allData || false
 
 // ** Connect to database. 
   require('./../config/db.config') //r u sure?
-// if(allData){
-//   console.log(`Okey, dude, we will search in the ${postalCode.pc.length} postal codes from Madrid.`);
-//   [28045].forEach((where, index) => {
-//     setTimeout(function(){
-//       getPlaces(where)
-//     }, index * 5000);    
-//   })
-// } else {
+
+if(!allData){
+  console.log(`Okey, dude, we will search in ${postalCode.pc.length} postal codes from Madrid.`);
+  postalCode.pc.forEach((where, index) => {
+    setTimeout(function(){
+      getPlaces(where)
+        .then(status => console.log(`Imported: ${status} for ${where}`))
+    }, index * 6000);    
+  })
+} else {
   // console.log(`we are looking for ${what} in ${where} and we will look for more ${all}`) 
   getPlaces()
     .then(status => console.log(`Imported: ${status}`))
     .catch(err => console.log(`err: ${err}`))
-// }
+}
 
 
 function getPlaces (where = 28045, what='Restaurants', output='json') {
@@ -43,13 +45,13 @@ function getPlaces (where = 28045, what='Restaurants', output='json') {
               .then(sleeper())//sleeper para el tercer request
               .then(newData=>{
                 data = [...data, ...mapping(newData.data)]
-                console.log(`we found ${data.length} elements in the second page`)
+                console.log(`we found ${data.length} elements including the second page`)
                 if(newData.nxtToken){
                   return  (
                     moreData(newData.nxtToken)
                       .then(newData=>{
                         data = [...data, ...mapping(newData.data)]                                    
-                        console.log(`we found ${data.length} elements in the third page`)
+                        console.log(`we found ${data.length} elements including the third page`)
                         return importMongo(data).then(status=>status)
                       })    
                   )
@@ -69,10 +71,10 @@ function getPlaces (where = 28045, what='Restaurants', output='json') {
 const moreData = nxtToken => {
   return (
     axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json`, {         
-    params: {pagetoken : nxtToken, key : GOOGLE_API,}
+      params: {pagetoken : nxtToken, key : GOOGLE_API,}
     })
       .then(response => {
-        console.log(`Status: ${response.data.status}`)
+        // console.log(`Status: ${response.data.status}`)
         return {data: response.data.results, nxtToken: response.data.next_page_token}
       })
       .catch(err => console.log(err))
@@ -84,7 +86,7 @@ const moreData = nxtToken => {
 * Requesting the next page before it is available will return an INVALID_REQUEST response. 
 * Retrying the request with the same next_page_token will return the next page of results.
  */
-function sleeper(ms = 3000) {
+function sleeper(ms = 2000) {
   return function(x) {
     return new Promise(resolve => setTimeout(() => resolve(x), ms));
   };
