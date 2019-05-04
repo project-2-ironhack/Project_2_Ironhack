dd8500bab097178ef06ffdaac83ba89cfc28c8cb
Fernando Lacaba <fernando.lacaba.herrera@gmail.com>
2019-05-04 09:52:11 +0200

    Object assign done

--- controllers/dash.controller.js
+++ controllers/dash.controller.js
@@ -1,31 +1,16 @@
 const bbvaService = require('../services/bbva.service');
 
-// module.exports.getBbbvaData = (req,res,next) => {
-//   const params = req.body.params
-//   const bbvaTokenCall = new bbvaApi.BbvaTokenCall(BBVA_AUTH)
-//   axios(bbvaTokenCall)
-//     .then(response => {
-//       const accessToken = response.data['access_token']
-//       const tokenType = response.data['token_type']
-//       const bbvaApiCall = new bbvaApi.BbvaApiCall(accessToken,tokenType,params)
-//       return axios(bbvaApiCall)
-//         .then(response => res.json(response.data))
-//     })
-//     .catch(next)
-// }
-
-
 module.exports.display = (req,res,next) => {
   const promises = req.body.graphs.map((graph) => {
-    return bbvaService.getData({min: '201501', max: '201501'})
+    return bbvaService.getData({min: '201501', max: '201512'})
   })
 
   Promise.all(promises)
     .then((queries) => {
       const data = queries.map((info, i) => {
-        console.log(info)
-        return Object.create(info)});
-      console.log(data);
+        console.log(info[0])
+        return Object.assign( req.body.graphs[i],{data:info})});
+      console.log(data)
       res.render('dashboard/list', { dashboard:true, data:data })
     })
     .catch(error => next(error))
